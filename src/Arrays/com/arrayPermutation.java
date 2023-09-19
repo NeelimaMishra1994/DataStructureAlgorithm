557948f845863fffad7b8412fdcd12d93673257c
neelima <neelimamishra172@gmail.com>
2023-09-19 22:10:52 +0100

    ArrayPermutation

--- src/Arrays/com/arrayPermutation.java
+++ src/Arrays/com/arrayPermutation.java
@@ -1,9 +1,17 @@
 package Arrays.com;
 
+import java.util.Arrays;
+
 public class arrayPermutation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] nums= {0,2,1,5,3,4};
+		int[] ans = new int[7];
+		for(int i =0; i<nums.length; i++) {
+			ans[i] = nums[nums[i]];
+		}
+		System.out.println(Arrays.toString(ans));
 
 	}
 
